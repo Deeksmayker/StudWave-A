aed7a9a278db4d0f2a381e882ec12693954672b6
Katarsis <63051076+Deeksmayker@users.noreply.github.com>
2021-11-01 02:32:29 +0500

    Quest characters and questChain system

---
+++ Assets/Scripts/Quests/QuestChainIds.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Quests
+{
+    public static class QuestChainIds
+    {
+        public static string Test1 { get; } = "test1";
+        public static string Test2 { get; } = "test2";
+        public static string Aboba { get; } = "ABOBA";
+    }
+}
--- Assets/Scripts/Quests/QuestIds.cs
+++ Assets/Scripts/Quests/QuestIds.cs
@@ -8,7 +8,11 @@ namespace Assets.Scripts.Quests
 {
     public static class QuestIds
     {
-        public static string Test1 { get; } = "test1";
-        public static string Test2 { get; } = "test2";
+        public static string PaperCourier { get; } = "Бумажный куряга";
+        public static string HelpBabka { get; } = "Помоги бабабульке";
+
+        public static string AskAboba { get; } = "Спросить у абобы помощи";
+        public static string TellStudPermission { get; } = "Сказать студсовету условия помощи от абобы";
+        public static string ReturnToAboba { get; } = "Вернуться к абобе";
     }
 }
